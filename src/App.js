4802c9003095c0e1ce1b3f76144bff214a684733
SajSultan <ksa.sultan1979@yahoo.com>
2023-03-25 22:55:03 +0000

    test chart dat a

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import CryptoTicker from './components/CryptoTicker';
 import ChatBot from './components/ChatBot';
 import CryptoDisplay from './components/CryptoDisplay';
 import ParticlesBg from './components/BackgroundAnimation';
+import CryptoChart from './components/CryptoChart';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <CryptoTicker />
       <CryptoDisplay />
       <ChatBot />
+      <CryptoChart />
     </div>
   );
 }
--- src/components/CryptoChart.jsx
+++ src/components/CryptoChart.jsx
@@ -1,9 +1,5 @@
-// import '.CryptoChart.css';
+// import '../assets/styles/CryptoChart.css';
 import React from "react";
-import { Chart as ChartJS, LineElement } from "react-chartjs-2";
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { Line } from "react-chartjs-2";
 import {
   Chart as ChartJS,
   LineElement,
@@ -12,7 +8,10 @@ import {
   PointElement,
   Legend,
   Tooltip,
-} from "chart.js";
+} from "react-chartjs-2";
+// import { useEffect, useState } from "react";
+// import axios from "axios";
+import { Line } from "react-chartjs-2";
 
 ChartJS.register(
   LineElement,
@@ -23,13 +22,47 @@ ChartJS.register(
   Tooltip
 );
 const CryptoChart = () => {
+  // will be api call for chart data . test for now
+
+  const data = {
+    labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+    datasets: [
+      {
+        label: "Sales of the Week",
+        data: [3, 6, 9],
+        backgroundColor: "aqua",
+        borderColor: "black",
+        pointBorderColor: "aqua",
+        fill: true,
+        tension: 0.3
+      },
+    ],
+  };
+
+  // options for legend and scales
+  const options = {
+    plugins: {
+      legend: true
+    },
+    scales: {
+      y: {
+
+      }
+    }
+  }
+
   return (
     <div className="CryptoChart">
-      <Line data={data} />
-      
-
+      <h1>Crypto Chart</h1>
+      <div style={
+        {width: "600px", height: "300px"}
+      }>
+      <Line 
+      data={data} 
+      options={options}
+      ></Line>
+      </div>
     </div>
   );
-
-}
+};
 export default CryptoChart;
