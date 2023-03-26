e10cc88318e577974eaf21740e35f76eca803da0
Jack Turner <34825609+neverekt@users.noreply.github.com>
2023-03-26 22:26:05 +0100

    small edits

--- src/assets/functions/fetchNews.js
+++ src/assets/functions/fetchNews.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 const cryptoPanicEndpoint = '/api/posts/';
 
-// const cryptoPanicEndpoint = 'https://cryptopanic.com/api/v1/posts/';
-
 //   auth_token: process.env.CRYPTOPANIC_API_KEY,
 // const auth_token = '1d5f85a24c81af8ec3b56e9c8cc71c179af39521';
 const auth_token = '5449109ce0aec89c6e9b7fa89d56364affafc3e2';
