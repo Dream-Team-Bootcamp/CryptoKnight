b54da8532a72979f50f9a30cb42a3c7a07551a76
Sean Fox <admin@drgunbot.com>
2023-03-27 07:31:41 +1100

    Quick backup before I turn my computer off

--- src/components/Frank.jsx
+++ src/components/Frank.jsx
@@ -1,7 +1,6 @@
 // This file defines the Chatbot component, which is a chat interface that allows users to communicate with an AI-powered assistant called Frank.
 import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { BsFillChatDotsFill } from 'react-icons/bs'; // Importing an icon from the react-icons/bs library
-import { IoClose, IoChevronDown, IoChevronUp, IoExpand } from 'react-icons/io5'; // Importing icons from the react-icons/io5 library
 import styled from '@emotion/styled'; // Importing a styled-component library
 import { motion, AnimatePresence } from 'framer-motion'; // Importing animation libraries
 import axios from 'axios';
@@ -60,7 +59,6 @@ const Chatbot = () => {
     setUserInput(e.target.value);
   };
 
-
   const chatWithFrank = useCallback(async (messages) => {
     try {
       const response = await axios.post('/api/chatWithFrank', { messages }, { timeout: 60000 });
@@ -70,19 +68,15 @@ const Chatbot = () => {
       return 'You killed Frank, you monster!';
     }
   }, []);
-  
 
 
   const handleSubmit = useCallback(
     async (e) => {
       e.preventDefault(); // Prevent the default form submission behavior
-
       if (userInput.trim() === '') return; // If the user input is empty or just whitespace, do nothing
-
       sendMessage(userInput, 'user'); // Add the user's input as a new message with the role of 'user'
       setUserInput(''); // Clear the user input state variable
       setIsTyping(true); // Set the isTyping state variable to true
-
       const response = await chatWithFrank([
         ...messages,
         { role: 'user', content: userInput },
@@ -127,17 +121,10 @@ const Chatbot = () => {
           <Header>
             <ButtonContainer>
               <ControlButton onClick={handleMinimize} red>
-                <IoClose size={14} />
               </ControlButton>
               <ControlButton onClick={handleResize} yellow>
-                {chatSize === 'maximized' ? (
-                  <IoChevronUp size={14} />
-                ) : (
-                  <IoChevronDown size={14} />
-                )}
               </ControlButton>
               <ControlButton onClick={handleMaximize} green>
-                <IoExpand size={14} />
               </ControlButton>
             </ButtonContainer>
             <TypingStatus>
---
+++ src/components/placeholder.js
@@ -0,0 +1,46 @@
+// Placeholder Component 1
+const Component1 = () => {
+    return <div style={{ background: 'lightblue', padding: '1rem' }}>Component 1</div>;
+  };
+  
+  // Placeholder Component 2
+  const Component2 = () => {
+    return <div style={{ background: 'lightgreen', padding: '1rem' }}>Component 2</div>;
+  };
+  
+  // Placeholder Component 3
+  const Component3 = () => {
+    return <div style={{ background: 'lightyellow', padding: '1rem' }}>Component 3</div>;
+  };
+  
+  // Placeholder Component 4
+  const Component4 = () => {
+    return <div style={{ background: 'lightcoral', padding: '1rem' }}>Component 4</div>;
+  };
+
+  // Placeholder Component 5
+const Component5 = () => {
+  return <div style={{ background: 'lavender', padding: '1rem' }}>Component 5</div>;
+};
+
+// Placeholder Component 6
+const Component6 = () => {
+  return <div style={{ background: 'palevioletred', padding: '1rem' }}>Component 6</div>;
+};
+
+// Placeholder Component 7
+const Component7 = () => {
+  return <div style={{ background: 'khaki', padding: '1rem' }}>Component 7</div>;
+};
+
+// Placeholder Component 8
+const Component8 = () => {
+  return <div style={{ background: 'skyblue', padding: '1rem' }}>Component 8</div>;
+};
+
+// Placeholder Component 9
+const Component9 = () => {
+  return <div style={{ background: 'plum', padding: '1rem' }}>Component 9</div>;
+};
+
+export { Component1, Component2, Component3, Component4, Component5, Component6, Component7, Component8, Component9 };
