0d05d335e7697f705b57d7e27dddf3cd2dca20d3
Sean Fox <admin@drgunbot.com>
2023-03-29 09:46:23 +1100

    Rearranged order

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import BackgroundAnimation from './components/BackgroundAnimation';
 import Frank from './components/Frank';
 import CryptoChart from "./components/CryptoChart";
 
+
+
 function App() {
   return (
     <React.Fragment>
