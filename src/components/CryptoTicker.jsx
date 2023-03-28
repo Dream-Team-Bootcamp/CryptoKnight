d3136d7d3ba99843118b6510fa8cb527ef50eb1c
Sean Fox <admin@drgunbot.com>
2023-03-29 09:47:08 +1100

    Tried using another version to extend the bar. Works, but doesn't look good

--- src/components/CryptoTicker.jsx
+++ src/components/CryptoTicker.jsx
@@ -161,3 +161,250 @@ const CryptoTicker = () => {
 };
 
 export default CryptoTicker;
+
+
+// import React, { useState, useCallback, useEffect } from 'react';
+// import { motion } from 'framer-motion';
+// import styled from '@emotion/styled';
+
+// const TickerContainer = styled.div`
+//     position: relative;
+//     display: flex;
+//     flex-direction: column;
+//     align-items: center;
+// `;
+
+// const ExpandButton = styled(motion.button)`
+//     position: fixed;
+//     top: 20px;
+//     right: 20px;
+//     z-index: 100;
+//     border: none;
+//     background-color: transparent;
+//     color: white;
+//     font-size: 24px;
+//     cursor: pointer;
+// `;
+
+// const InnerTicker = styled(motion.div)`
+//     display: flex;
+//     align-items: center;
+//     overflow-x: auto;
+//     width: 100%;
+//     height: 50px;
+// `;
+
+// const Coin = styled.div`
+//     display: flex;
+//     flex-direction: column;
+//     align-items: center;
+//     justify-content: center;
+//     margin: 0 10px;
+//     cursor: pointer;
+// `;
+
+// const CoinLogo = styled.img`
+//     width: 24px;
+//     height: 24px;
+//     margin-bottom: 5px;
+// `;
+
+// const CoinName = styled.span`
+//     font-size: 12px;
+//     text-align: center;
+// `;
+
+// const CoinPrice = styled.span`
+//     font-size: 12px;
+//     text-align: center;
+//     color: ${(props) => (props.isUp ? 'green' : 'red')};
+// `;
+
+// const CoinTooltip = styled.div`
+//     display: flex;
+//     flex-direction: column;
+//     align-items: center;
+//     justify-content: center;
+//     position: absolute;
+//     background-color: white;
+//     border-radius: 5px;
+//     box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+//     padding: 10px;
+//     width: 300px;
+//     height: auto;
+//     font-size: 12px;
+// `;
+
+// const CryptoTicker = () => {
+//     const [coins, setCoins] = useState([]);
+//     const [isDataLoaded, setIsDataLoaded] = useState(false);
+//     const [isTickerPaused, setIsTickerPaused] = useState(false);
+//     const [activeCoin, setActiveCoin] = useState(null);
+//     const [tooltipPosition, setTooltipPosition] = useState({
+//         top: 0,
+//         left: 0,
+//         width: 0,
+//         height: 'auto',
+//     });
+//     const [timeoutId, setTimeoutId] = useState(null);
+//     const [isExpanded, setIsExpanded] = useState(false);
+
+//     const fetchCoins = useCallback(async () => {
+//         const response = await fetch(
+//             'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+//         );
+//         const data = await response.json();
+//         setCoins(data);
+//         setIsDataLoaded(true);
+//     }, []);
+
+//     useEffect(() => {
+//         fetchCoins();
+//         const interval = setInterval(fetchCoins, 60000);
+//         return () => clearInterval(interval);
+//     }, [fetchCoins]);
+    
+//     const isPriceUp = (coin) => {
+//         return coin.price_change_percentage_24h > 0;
+//     };
+    
+//     const formatPrice = (price) => {
+//         if (price >= 1) {
+//             return `$${price.toFixed(2)}`;
+//         } else {
+//             return `$${price.toFixed(6)}`;
+//         }
+//     };
+    
+//     const openCoinChart = (coinName) => {
+//         window.open(`https://www.coingecko.com/en/coins/${coinName.toLowerCase()}`, '_blank');
+//     };
+    
+//     const handleMouseEnter = (coin, e) => {
+//         if (timeoutId) {
+//             clearTimeout(timeoutId);
+//             setTimeoutId(null);
+//         }
+//         setActiveCoin(coin.id);
+//         setIsTickerPaused(true);
+//         const target = e.target.closest('.coin');
+//         if (target) { // add null check here
+//             const targetRect = target.getBoundingClientRect();
+//             const tooltipWidth = target.offsetWidth;
+//             const tooltipLeft = targetRect.left;
+//             const tooltipTop = targetRect.bottom + window.scrollY;
+//             setTooltipPosition({
+//                 top: tooltipTop,
+//                 left: tooltipLeft,
+//                 width: tooltipWidth,
+//                 height: 'auto', // reset the height to auto on mouse enter
+//             });
+//         }
+//     };
+    
+    
+//     const handleMouseLeave = (e) => {
+//         if (e.relatedTarget && e.relatedTarget.classList && e.relatedTarget.classList.contains('coin-tooltip')) {
+//             return;
+//         }
+//         const id = setTimeout(() => {
+//             setIsTickerPaused(false);
+//             setActiveCoin(null);
+//         }, 500);
+//         setTimeoutId(id);
+//     };
+    
+//     const handleExpandButtonClick = () => {
+//         setIsExpanded(!isExpanded);
+//     };
+    
+//     const animationVariants = {
+//         expanded: {
+//             width: '100%',
+//             transition: {
+//                 duration: 0.5,
+//             },
+//         },
+//         collapsed: {
+//             width: '50%',
+//             transition: {
+//                 duration: 0.5,
+//             },
+//         },
+//     };
+    
+//     return (
+//         <TickerContainer>
+//             <ExpandButton onClick={handleExpandButtonClick} whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }}>
+//                 {isExpanded ? 'Minimize' : 'Expand'}
+//             </ExpandButton>
+//             <InnerTicker
+//                 variants={animationVariants}
+//                 animate={isExpanded ? 'expanded' : 'collapsed'}
+//                 transition={{ duration: 0.5 }}
+//                 onMouseLeave={handleMouseLeave}
+//             >
+//                 {coins.map((coin) => (
+//                     <Coin
+//                         key={coin.id}
+//                         onClick={() => openCoinChart(coin.name)}
+//                         onMouseEnter={(e) => handleMouseEnter(coin, e)}
+//                         onMouseLeave={handleMouseLeave}
+//                     >
+//                         <CoinLogo src={coin.image} alt={coin.name} />
+//                         <CoinName>{coin.name}</CoinName>
+//                         <CoinPrice isUp={isPriceUp(coin)}>
+//                             {formatPrice(coin.current_price)} ({coin.price_change_percentage_24h.toFixed(2)}%)
+//                         </CoinPrice>
+//                     </Coin>
+//                 ))}
+//             </InnerTicker>
+//             {isTickerPaused && <div className="ticker-paused-overlay">Paused</div>}
+//             {coins.map(
+//                 (coin) =>
+//                     activeCoin === coin.id && (
+//                         <CoinTooltip
+//                             key={coin.id}
+//                             style={{
+//                                 display: 'block',
+//                                 top: tooltipPosition.top,
+//                                 left: tooltipPosition.left,
+//                                 width: tooltipPosition.width,
+//                                 height: tooltipPosition.height,
+//                             }}
+//                             onMouseEnter={() => {
+//                                 if (timeoutId) {
+//                                     clearTimeout(timeoutId);
+//                                     setTimeoutId(null);
+//                                 }
+//                                 setIsTickerPaused(true);
+//                             }}
+//                             onMouseLeave={handleMouseLeave}
+//                         >
+//                             <p>Market Cap: {coin.market_cap.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</p>
+//                             <p>Total Volume: {coin.total_volume.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</p>
+//                             <p>24h High: {formatPrice(coin.high_24h)}</p>
+//                             <p>24h Low: {formatPrice(coin.low_24h)}</p>
+//                             <p>24h Open: {formatPrice(coin.current_price - coin.price_change_24h)}</p>
+//                             <p>24h Close: {formatPrice(coin.current_price)}</p>
+//                             <p>Market Cap Rank: {coin.market_cap_rank}</p>
+//                             <p>Circulating Supply: {coin.circulating_supply.toLocaleString('en-US')}</p>
+//                             <p>Total Supply: {coin.total_supply ? coin.total_supply.toLocaleString('en-US') : 'N/A'}</p>
+//                             <p>Max Supply: {coin.max_supply ? coin.max_supply.toLocaleString('en-US') : 'N/A'}</p>
+//                             <p>ATH: {formatPrice(coin.ath)}</p>
+//                             <p>ATH Date: {new Date(coin.ath_date).toLocaleDateString()}</p>
+//                             <p>ATL: {formatPrice(coin.atl)}</p>
+//                             <p>ATL Date: {new Date(coin.atl_date).toLocaleDateString()}</p>
+//                             <p>Market Cap Change (24h): {coin.market_cap_change_percentage_24h}%</p>
+//                             <p>All Time High Change (24h): {coin.ath_change_percentage}%</p>
+//                             <p>All Time Low Change (24h): {coin.atl_change_percentage}%</p>
+//                             <p>Last Updated: {new Date(coin.last_updated).toLocaleString()}</p>
+//                         </CoinTooltip>
+//                     )
+//             )}
+//         </TickerContainer>
+//     );
+// };
+
+// export default CryptoTicker;    
+                        
