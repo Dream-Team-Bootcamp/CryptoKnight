400cc2c5f9fbc75d32cdb406f4f14fa309a48074
Sean Fox <admin@drgunbot.com>
2023-03-29 15:38:13 +1100

    Made nav bar visible - affects the scrollbar - checking out another branch where I fixed the issue of a bad tooltip dropdown

---
+++ src/components/Placeholder2.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div style={{ backgroundColor: '#D3D3D3', height: '100vh' }}>
+      About
+    </div>
+  );
+}
+
+export default About;
